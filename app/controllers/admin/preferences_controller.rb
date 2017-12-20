a93c6b1b38a1f426dc78df1c8f18a88d571f170c
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-20 20:42:46 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,10 +1,11 @@
 class Admin::PreferencesController < ApplicationController
 
+  def new
+    @prefs = Preference.new
+  end
+    
   def show
     @prefs = Preference.find(params[:id])
   end
 
-  def new
-    @prefs = Preference.new
-  end
 end
