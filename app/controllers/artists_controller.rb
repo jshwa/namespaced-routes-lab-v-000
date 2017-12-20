3f2e4b6e8ee12d669eb9e65c0eeff3032d0fab35
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-20 20:25:20 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if Preferences.allow_create_artist == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
