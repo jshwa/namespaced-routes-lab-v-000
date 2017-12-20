df6d7928c1f7f057a7232e2c7be4b6d5355a7939
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-20 21:08:57 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -13,4 +13,9 @@ class Admin::PreferencesController < ApplicationController
     redirect_to admin_preference_path(@prefs)
   end
 
+  private
+
+  def preference_params
+    params.require(:preference).permit(:artist_sort_order, :song_sort_order, :allow_create_artists, :allow_create_songs)
+  end
 end
