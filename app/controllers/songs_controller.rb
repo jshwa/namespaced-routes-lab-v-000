d93a2627cffbaaa7211ae83995241f0c7fe6c277
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-21 00:39:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,12 +7,12 @@ class SongsController < ApplicationController
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
-      elsif @preference && @preference.song_sort_order
-          @songs = @artist.songs.order(title: @preference.song_sort_order)
+      elsif @preferences && @preferences.song_sort_order
+          @songs = @artist.songs.order(title: @preferences.song_sort_order)
       else
         @songs = @artist.songs
       end
-    elsif @preference && @preference.song_sort_order
+    elsif @preferences && @preferences.song_sort_order
       @songs = Song.order(title: @preferences.song_sort_order)
     else
       @songs = Song.all
@@ -32,7 +32,7 @@ class SongsController < ApplicationController
   end
 
   def new
-    if @preference.allow_create_songs == true
+    if @preferences.allow_create_songs == true
       @song = Song.new
     else
       redirect_to songs_path
